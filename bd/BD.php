9abd0c5e43ca1a1e6bc7d373c64c8333f47c4892
Nicolas De Cabrera <alfredonicolasdecabrera@gmail.com>
2022-05-09 21:55:02 -0300

    se agrego la funcionalidad de recibir jsonpor body para hacer el CRUD  y enviar respuestas 404 si el producto no existe

--- bd/BD.php
+++ bd/BD.php
@@ -1,79 +1,132 @@
 <?php
-$pdo=null;
-$host="localhost";
-$user="root";
-$password="Admin123";
-$bd="tutoriales";
+$pdo = null;
+$host = "localhost";
+$user = "root";
+$password = "root";
+$bd = "prueba";
 
-function conectar(){
-    try{
-        $GLOBALS['pdo']=new PDO("mysql:host=".$GLOBALS['host'].";dbname=".$GLOBALS['bd']."", $GLOBALS['user'], $GLOBALS['password']);
+function conectar()
+{
+    try {
+        $GLOBALS['pdo'] = new PDO("mysql:host=" . $GLOBALS['host'] . ";dbname=" . $GLOBALS['bd'] . "", $GLOBALS['user'], $GLOBALS['password']);
         $GLOBALS['pdo']->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    }catch (PDOException $e){
-        print "Error!: No se pudo conectar a la bd ".$bd."<br/>";
-        print "\nError!: ".$e."<br/>";
+    } catch (PDOException $e) {
+        print "Error!: No se pudo conectar a la bd <br/>";
+        print "\nError!: " . $e . "<br/>";
         die();
     }
 }
 
-function desconectar() {
-    $GLOBALS['pdo']=null;
+function desconectar()
+{
+    $GLOBALS['pdo'] = null;
 }
 
-function metodoGet($query){
-    try{
+function metodoGet($query)
+{
+    try {
         conectar();
-        $sentencia=$GLOBALS['pdo']->prepare($query);
+        $sentencia = $GLOBALS['pdo']->prepare($query);
         $sentencia->setFetchMode(PDO::FETCH_ASSOC);
         $sentencia->execute();
         desconectar();
         return $sentencia;
-    }catch(Exception $e){
-        die("Error: ".$e);
+    } catch (Exception $e) {
+        die("Error: " . $e);
     }
 }
 
-function metodoPost($query, $queryAutoIncrement){
-    try{
+function metodoPost($query, $queryAutoIncrement)
+{
+    try {
         conectar();
-        $sentencia=$GLOBALS['pdo']->prepare($query);
+        $sentencia = $GLOBALS['pdo']->prepare($query);
         $sentencia->execute();
-        $idAutoIncrement=metodoGet($queryAutoIncrement)->fetch(PDO::FETCH_ASSOC);
-        $resultado=array_merge($idAutoIncrement, $_POST);
-        $sentencia->closeCursor();
+        $id = metodoGet($queryAutoIncrement)->fetch(PDO::FETCH_COLUMN);
         desconectar();
-        return $resultado;
-    }catch(Exception $e){
-        die("Error: ".$e);
+        conectar();
+        $data = metodoGet("SELECT * FROM frameworks WHERE id='$id'")->fetch(PDO::FETCH_ASSOC);
+        desconectar();
+        return $data;
+    } catch (Exception $e) {
+        die("Error: " . $e);
     }
 }
 
 
-function metodoPut($query){
-    try{
+function metodoPut($query, $id)
+{
+    try {
         conectar();
-        $sentencia=$GLOBALS['pdo']->prepare($query);
-        $sentencia->execute();
-        $resultado=array_merge($_GET, $_POST);
-        $sentencia->closeCursor();
-        desconectar();
-        return $resultado;
-    }catch(Exception $e){
-        die("Error: ".$e);
-    }
-}
-
-function metodoDelete($query){
-    try{
-        conectar();
-        $sentencia=$GLOBALS['pdo']->prepare($query);
+        $sentencia = $GLOBALS['pdo']->prepare($query);
         $sentencia->execute();
         $sentencia->closeCursor();
         desconectar();
-        return $_GET['id'];
-    }catch(Exception $e){
-        die("Error: ".$e);
+        conectar();
+        $data = metodoGet("SELECT * FROM frameworks WHERE id='$id'")->fetch(PDO::FETCH_ASSOC);
+        desconectar();
+        return $data;
+    } catch (Exception $e) {
+        die("Error: " . $e);
     }
 }
 
-?>
+function metodoDelete($query, $id)
+{
+    try {
+        conectar();
+        $sentencia = $GLOBALS['pdo']->prepare($query);
+        $sentencia->execute();
+        $sentencia->closeCursor();
+        desconectar();
+        return $id;
+    } catch (Exception $e) {
+        die("Error: " . $e);
+    }
+}
+
+//FORM-DATA
+
+// function metodoPost($query, $queryAutoIncrement){
+//     try{
+//         conectar();
+//         $sentencia=$GLOBALS['pdo']->prepare($query);
+//         $sentencia->execute();
+//         $idAutoIncrement=metodoGet($queryAutoIncrement)->fetch(PDO::FETCH_ASSOC);
+//         $resultado=array_merge($idAutoIncrement, $_POST);
+//         $sentencia->closeCursor();
+//         desconectar();
+//         return $resultado;
+//     }catch(Exception $e){
+//         die("Error: ".$e);
+//     }
+// }
+
+// function metodoPut($query)
+// {
+//     try {
+//         conectar();
+//         $sentencia = $GLOBALS['pdo']->prepare($query);
+//         $sentencia->execute();
+//         $resultado = array_merge($_GET, $_POST);
+//         $sentencia->closeCursor();
+//         desconectar();
+//         return $resultado;
+//     } catch (Exception $e) {
+//         die("Error: " . $e);
+//     }
+// }
+
+// function metodoDelete($query)
+// {
+//     try {
+//         conectar();
+//         $sentencia = $GLOBALS['pdo']->prepare($query);
+//         $sentencia->execute();
+//         $sentencia->closeCursor();
+//         desconectar();
+//         return $_GET['id'];
+//     } catch (Exception $e) {
+//         die("Error: " . $e);
+//     }
+// }
--- index.php
+++ index.php
@@ -4,57 +4,123 @@ include 'bd/BD.php';
 
 header('Access-Control-Allow-Origin: *');
 
-if($_SERVER['REQUEST_METHOD']=='GET'){
-    if(isset($_GET['id'])){
-        $query="select * from frameworks where id=".$_GET['id'];
-        $resultado=metodoGet($query);
+$json =  file_get_contents('php://input'); //rescata datos del body del mensaje
+$data = json_decode($json); //convierte esos daos en objeto
+
+//ATRIBUTOS DEL PRODUCTO
+$METHOD = $data->METHOD;
+$id = $data->id;
+$nombre = $data->nombre;
+$lanzamiento = $data->lanzamiento;
+$desarrollador = $data->desarrollador;
+
+//GET
+//ALL:http://localhost/apiPhpPrueba/
+//ID: http://localhost/apiPhpPrueba/?id=4
+if ($_SERVER['REQUEST_METHOD'] == 'GET') {
+    if (isset($_GET['id'])) {
+        $query = "select * from frameworks where id=" . $_GET['id'];
+        $resultado = metodoGet($query);
         echo json_encode($resultado->fetch(PDO::FETCH_ASSOC));
-    }else{
-        $query="select * from frameworks";
-        $resultado=metodoGet($query);
-        echo json_encode($resultado->fetchAll()); 
+    } else {
+        $query = "select * from frameworks";
+        $resultado = metodoGet($query);
+        echo json_encode($resultado->fetchAll());
     }
     header("HTTP/1.1 200 OK");
     exit();
 }
 
-if($_POST['METHOD']=='POST'){
-    unset($_POST['METHOD']);
-    $nombre=$_POST['nombre'];
-    $lanzamiento=$_POST['lanzamiento'];
-    $desarrollador=$_POST['desarrollador'];
-    $query="insert into frameworks(nombre, lanzamiento, desarrollador) values ('$nombre', '$lanzamiento', '$desarrollador')";
-    $queryAutoIncrement="select MAX(id) as id from frameworks";
-    $resultado=metodoPost($query, $queryAutoIncrement);
-    echo json_encode($resultado);
+//POST
+if ($METHOD == 'POST') {
+    unset($data->METHOD);
+    $query = "insert into frameworks(nombre, lanzamiento, desarrollador) values ('$nombre', '$lanzamiento', '$desarrollador')";
+    $queryAutoIncrement = "select MAX(id) as id from frameworks";
+    $db_data = metodoPost($query, $queryAutoIncrement);
+    if ($db_data) {
+        echo json_encode($db_data);
+        header("HTTP/1.1 200 OK");
+    } else {
+        echo '{"error":"no se agrego"}';
+        header("HTTP/1.1 400 Bad Request");
+    }
+    exit();
+}
+//PUT
+if ($METHOD == 'PUT') {
+    unset($data->METHOD);
+    $query = "UPDATE frameworks SET nombre='$nombre', lanzamiento='$lanzamiento', desarrollador='$desarrollador' WHERE id='$id'";
+    $db_data = metodoPut($query, $id);
+    if ($db_data) {
+        echo json_encode($db_data);
+        header("HTTP/1.1 200 OK");
+    } else {
+        echo '{"error":"no existe"}';
+        header("HTTP/1.1 400 Bad Request");
+    }
+    exit();
+}
+//DELETE
+if ($METHOD == 'DELETE') {
+    unset($data->METHOD);
+    $query = "DELETE FROM frameworks WHERE id='$id'";
+    $resultado = metodoDelete($query, $id);
+    echo '{"id":"' . $resultado . '"}';
     header("HTTP/1.1 200 OK");
     exit();
 }
 
-if($_POST['METHOD']=='PUT'){
-    unset($_POST['METHOD']);
-    $id=$_GET['id'];
-    $nombre=$_POST['nombre'];
-    $lanzamiento=$_POST['lanzamiento'];
-    $desarrollador=$_POST['desarrollador'];
-    $query="UPDATE frameworks SET nombre='$nombre', lanzamiento='$lanzamiento', desarrollador='$desarrollador' WHERE id='$id'";
-    $resultado=metodoPut($query);
-    echo json_encode($resultado);
-    header("HTTP/1.1 200 OK");
-    exit();
-}
+//FORM-DATA
 
-if($_POST['METHOD']=='DELETE'){
-    unset($_POST['METHOD']);
-    $id=$_GET['id'];
-    $query="DELETE FROM frameworks WHERE id='$id'";
-    $resultado=metodoDelete($query);
-    echo json_encode($resultado);
-    header("HTTP/1.1 200 OK");
-    exit();
-}
+
+//POST
+// if($_POST['METHOD']=='POST'){
+//     unset($_POST['METHOD']);
+//     $nombre=$_POST['nombre'];
+//     $lanzamiento=$_POST['lanzamiento'];
+//     $desarrollador=$_POST['desarrollador'];
+//     $query="insert into frameworks(nombre, lanzamiento, desarrollador) values ('$nombre', '$lanzamiento', '$desarrollador')";
+//     $queryAutoIncrement="select MAX(id) as id from frameworks";
+//     $resultado=metodoPost($query, $queryAutoIncrement);
+//     echo json_encode($resultado);
+// header("HTTP/1.1 200 OK");
+//     exit();
+// }
+
+//PUT
+// if ($_POST['METHOD'] == 'PUT') {
+//     unset($_POST['METHOD']);
+//     $id = $_GET['id'];
+//     $nombre = $_POST['nombre'];
+//     $lanzamiento = $_POST['lanzamiento'];
+//     $desarrollador = $_POST['desarrollador'];
+//     $query = "UPDATE frameworks SET nombre='$nombre', lanzamiento='$lanzamiento', desarrollador='$desarrollador' WHERE id='$id'";
+//     $resultado = metodoPut($query);
+//     echo json_encode($resultado);
+//     header("HTTP/1.1 200 OK");
+//     exit();
+// }
+
+
+//DELETE
+// if ($_POST['METHOD'] == 'DELETE') {
+//     unset($_POST['METHOD']);
+//     $id = $_GET['id'];
+//     $query = "DELETE FROM frameworks WHERE id='$id'";
+//     $resultado = metodoDelete($query);
+//     echo json_encode($resultado);
+//     header("HTTP/1.1 200 OK");
+//     exit();
+// }
+
+// if ($_POST['METHOD'] == 'DELETE') {
+//     unset($_POST['METHOD']);
+//     $id = $_GET['id'];
+//     $query = "DELETE FROM frameworks WHERE id='$id'";
+//     $resultado = metodoDelete($query);
+//     echo json_encode($resultado);
+//     header("HTTP/1.1 200 OK");
+//     exit();
+// }
 
 header("HTTP/1.1 400 Bad Request");
-
-
-?>
